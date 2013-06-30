f549f9094240f84fbfda82c545f85ddb6bd0c045
chocoby <chocoby@gmail.com>
2013-06-30 17:44:15 +0900

    link_to_new_window に名前を変更

--- lib/helpers/link_to.rb
+++ lib/helpers/link_to.rb
@@ -1,5 +1,5 @@
 module LinkTo
-  def link_to_target_blank(text, target, attributes = {})
+  def link_to_new_window(text, target, attributes = {})
     attributes.merge! target: "_blank"
     Nanoc::Helpers::LinkTo.link_to(text, target, attributes)
   end
